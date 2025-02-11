9ce70f7315ca1754f22c84a0a6b59d23af40d739
rafihamid664 <unexplained664@gmail.com>
2025-02-11 22:54:42 +0700

    first commit

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,16 @@
+<?php
+$nama = "Rafi";
+$email = "unexplained664@gmail.com";
+$pekerjaan = "mahasiswa";
+$jurusan = "teknik informatika";
+$universitas = "unpas";
+
+
+echo "<h1>$nama</h1>";
+
+echo "<h4>$email</h4>";
+
+echo "<p>Nama saya $nama, saya sebagai $pekerjaan di $universitas, jurusan $jurusan</p>";
+
+
+?>
---
+++ tugas/1b.php
@@ -0,0 +1,17 @@
+<?php
+$angka = 56;
+
+echo "<strong>Aku adalah angka $angka</strong><br>";
+
+$angka *= 5;
+echo "Jika aku dikali 5, maka aku sekarang menjadi <strong>$angka</strong><br>";
+
+$angka /= 2;
+echo "Jika aku dibagi 2, maka aku sekarang menjadi <strong>$angka</strong><br>";
+
+$angka += 75;
+echo "Jika aku ditambah 75, maka aku sekarang menjadi <strong>$angka</strong><br>";
+
+$angka -= 20;
+echo "Jika aku dikurang 20, maka aku sekarang menjadi <strong>$angka</strong><br>";
+?>
